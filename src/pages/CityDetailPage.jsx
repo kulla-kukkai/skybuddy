8d67fb3018573a1965c3444523ec87d09887dd78
kulla-kukkai <kuk.kullavanee@gmail.com>
2026-09-22 22:19:01 +0200

    Add data fetching and back button to CityDetailPage

--- src/pages/CityDetailPage.jsx
+++ src/pages/CityDetailPage.jsx
@@ -1,9 +1,49 @@
-import { useParams } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useParams, Link } from "react-router-dom";
+import { getWeatherByCity } from "../services/weatherApi";
 
 function CityDetailPage() {
     const { cityName } = useParams();
 
-    return <div>Details: {cityName}</div>;
+    const [city, setCity] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        async function fetchCity() {
+        setIsLoading(true);
+        setError(null);
+
+        try {
+            const result = await getWeatherByCity(cityName);
+            setCity(result);
+        } catch (err) {
+            setError(err.message);
+        } finally {
+            setIsLoading(false);
+        }
+        }
+
+        fetchCity();
+    }, [cityName]);
+
+    if (isLoading) return <p>Loading...</p>;
+    if (error) return <p>Something went wrong, please try again later.</p>;
+
+    return (
+    <div>
+        <Link to="/">← Back to Home</Link>
+
+        {isLoading && <p>Loading...</p>}
+        {error && <p>Something went wrong, please try again later.</p>}
+        {!isLoading && !error && city && (
+            <>
+            <h1>{city.name}</h1>
+            <p>Temperature: {city.current.temperature}°C</p>
+            </>
+        )}
+    </div>
+    );
 }
 
 export default CityDetailPage;
